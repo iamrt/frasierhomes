c66558d1e0e6e6d205bd61447039dd34f974464b
iamrt <rance@ranceturley.com>
2015-10-25 21:17:53 -0500

    Mock-up nearly done. Need to change @768 styling for .content and add gallery for mobile

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-alert(window.innerWidth);
+$(function() {
+
+/***************Mobile Menu***************/
+
+	$('#mobile-menu').on('click', function() {
+		$('nav ul').fadeToggle(600)
+		$('button').toggleClass('change-bg')
+		$('.icon-bar').toggleClass('change-icon');
+	});
+
+
+});
